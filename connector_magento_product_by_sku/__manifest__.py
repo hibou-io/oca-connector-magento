562b4b5bdd610d3085e933fcab2893f6f1ac1c10
Jared Kipe <jared@jaredkipe.com>
2018-09-23 14:25:14 -0700

    ADD `connector_magento_product_by_sku` for 11.0

---
+++ connector_magento_product_by_sku/__manifest__.py
@@ -0,0 +1,14 @@
+{
+ 'name': 'Magento Connector - Product by SKU',
+ 'version': '10.0.1.0.0',
+ 'author': 'Hibou,Odoo Community Association (OCA)',
+ 'license': 'AGPL-3',
+ 'category': 'Hidden',
+ 'depends': [
+  'connector_magento',
+ ],
+ 'website': 'https://hibou.io/',
+ 'data': [],
+ 'installable': True,
+ 'auto_install': False,
+}
