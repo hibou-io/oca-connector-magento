6f79f92466ed77250a2bed47cf0c096fb430f560
Jared Kipe <jared@hibou.io>
2025-07-17 16:16:37 +0000

    [MIG] connector_magento_product_by_sku: to 15.0

--- connector_magento_product_by_sku/__manifest__.py
+++ connector_magento_product_by_sku/__manifest__.py
@@ -1,6 +1,6 @@
 {
  'name': 'Magento Connector - Product by SKU',
- 'version': '14.0.1.0.0',
+ 'version': '15.0.1.0.0',
  'author': 'Hibou,Odoo Community Association (OCA)',
  'license': 'AGPL-3',
  'category': 'Hidden',
